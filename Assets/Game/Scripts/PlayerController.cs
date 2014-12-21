c60d378fa7735df7beb1626f53d1578935c8bef5
Halvor <stektpotet@gmail.com>
2014-12-21 01:01:52 +0100

    Basic Movement

---
+++ Assets/Game/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+//	public AnimationControl animCtrl;
+
+	public float maxSpeed, jumpForce, acc;
+
+	void Start () 
+	{
+		
+	}
+
+	void Update () 
+	{
+		if(Input.GetKeyDown(KeyCode.E))
+		{
+			transform.Rotate(new Vector3( 0, 180));
+		}
+		if(Input.GetKeyDown(KeyCode.D))
+		{
+			rigidbody2D.AddForce(new Vector2(0,jumpForce));
+		}
+	}
+
+	void FixedUpdate () 
+	{
+		if(Input.GetKey(KeyCode.W))
+		{
+			if(transform.InverseTransformVector(rigidbody2D.velocity).x < maxSpeed)
+			{
+				rigidbody2D.AddForce(transform.right*acc);
+			}
+		}
+	}
+}

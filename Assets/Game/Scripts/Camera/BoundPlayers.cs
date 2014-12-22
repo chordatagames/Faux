a7259c0b8df446fef1388224a983fe1c690d690f
Halvor <stektpotet@gmail.com>
2014-12-23 00:23:10 +0100

    Camera Controllers
    Containing all "players" within the camera bounds and moving the camera
    according to all "players" position.

---
+++ Assets/Game/Scripts/Camera/BoundPlayers.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Camera))]
+public class BoundPlayers : MonoBehaviour 
+{
+	GameObject[] tracking;
+
+
+
+	public float margin= 0.5f;
+
+	void Awake()
+	{
+		if(camera.orthographic)
+		{
+			camera.orthographic = true;
+		}
+	}
+
+	void Start () 
+	{
+		tracking = GameObject.FindGameObjectsWithTag("Player");
+	}
+	
+	void LateUpdate () 
+	{
+		CenterCamera();
+		Encapsulate();
+	}
+
+	void CenterCamera()
+	{
+		Vector2 camPos = Vector2.zero;
+		foreach (GameObject p in tracking)
+		{
+			camPos += (Vector2)p.transform.position;
+		}
+		transform.position = camPos/2;
+	}
+
+	void Encapsulate()
+	{
+		float longestDist = 0;
+		Vector2 furthestAway = Vector2.zero;
+		foreach (GameObject p in tracking)
+		{
+			float dist = (p.transform.position - transform.position).magnitude;
+			if ( dist > longestDist)
+			{
+				longestDist = dist;
+				furthestAway = p.transform.position;
+			}
+		}
+		camera.orthographicSize = (furthestAway-(Vector2)transform.position).magnitude;
+
+	}
+}

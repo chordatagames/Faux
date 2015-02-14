ee8a8d692b4373ded3e54dedfbb63e2595678946
Froff <Fridtjof_Eik@hotmail.com>
2015-02-14 18:42:11 +0100

    Added black holes. Work will continue on them

---
+++ Assets/Resources/Game/Scripts/Gameplay/BlackHole.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlackHole : MonoBehaviour
+{
+	public GameObject whiteHole;
+
+	void OnTriggerEnter2D (Collider2D col)
+	{
+		col.transform.position = whiteHole.transform.position;
+	}
+}

d0f6298c955ad20d3273bef2d904ea899a9c765a
Froff <Fridtjof_Eik@hotmail.com>
2015-02-16 02:29:09 +0100

    More blapck hole functionality

--- Assets/Resources/Game/Scripts/Gameplay/BlackHole.cs
+++ Assets/Resources/Game/Scripts/Gameplay/BlackHole.cs
@@ -5,8 +5,22 @@ public class BlackHole : MonoBehaviour
 {
 	public GameObject whiteHole;
 
+	void Start ()
+	{
+		rigidbody2D.AddTorque (5.0f);
+	}
+
 	void OnTriggerEnter2D (Collider2D col)
 	{
 		col.transform.position = whiteHole.transform.position;
+		RotateTransform (col.transform);
+	}
+
+	void RotateTransform (Transform a)
+	{
+		Vector2 _velocity;
+
+
+		a.rigidbody2D.velocity = _velocity;
 	}
 }

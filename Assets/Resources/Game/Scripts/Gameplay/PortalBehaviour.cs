8cb05453aedefe1f8cd545ea6e175c523980d3ec
Froff <Fridtjof_Eik@hotmail.com>
2015-03-01 01:39:26 +0100

    Listen, pal. Portals don't need to actually work to be fun

---
+++ Assets/Resources/Game/Scripts/Gameplay/PortalBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PortalBehaviour : MonoBehaviour
+{
+	public GameObject otherPortal;
+	
+	List<GameObject> inside = new List<GameObject>();
+
+	void Update ()
+	{
+
+	}
+
+	void OnTriggerEnter2D (Collider2D col)
+	{
+		if (col.name != "portal_clone" && !inside.Contains(col.gameObject))
+		{
+			GameObject entering = col.transform.gameObject;
+			Debug.Log (entering.ToString());
+			inside.Add (entering);
+
+			MonoBehaviour[] cloneScripts = GetComponents<MonoBehaviour>();
+			foreach (MonoBehaviour script in cloneScripts)
+			{
+				Destroy (script);
+			}
+
+			Object temp = Instantiate (entering, otherPortal.transform.position, Quaternion.identity);
+			temp.name = "portal_clone";
+		}
+	}
+
+	void OnTriggerExit2D (Collider2D col)
+	{
+		inside.Remove (col.gameObject);
+	}
+}
--- Assets/Resources/Game/Scripts/SceneScripts/AllignPlanetObjects.cs
+++ Assets/Resources/Game/Scripts/SceneScripts/AllignPlanetObjects.cs
@@ -10,7 +10,9 @@ public class AllignPlanetObjects : MonoBehaviour
 		foreach(Transform child in transform)//access children
 		{
 			Vector2 fromCenterDir = (child.position - transform.position).normalized;
-			child.position = (Vector2)transform.position + fromCenterDir*(transform.localScale.x/2);
+
+			if (child.name != "OrangePortal" && child.name != "BluePortal")	//TODO: Make better system for this
+				child.position = (Vector2)transform.position + fromCenterDir*(transform.localScale.x/2);
 
 			float angle = Vector2.Angle(Vector2.right, -fromCenterDir)*Mathf.Deg2Rad*Mathf.Sign(-fromCenterDir.y) + Mathf.PI/2;
 			child.transform.rotation = new Quaternion(0, 0, 1 * Mathf.Sin (angle/2), Mathf.Cos (angle/2));

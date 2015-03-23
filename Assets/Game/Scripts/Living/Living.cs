54749895b1e8f6657b73e845596b5349485bf3eb
picklemagnet <infinitefacepalm@gmail.com>
2015-03-23 20:07:39 +0100

    Initial work of singleplayer mode has started.
    BE IMPRESSED BY MY BLOB

--- Assets/Game/Scripts/Living/Living.cs
+++ Assets/Game/Scripts/Living/Living.cs
@@ -3,28 +3,50 @@ using System.Collections;
 
 public abstract class Living : GameComponent 
 {
-	public float health = 100;
+	public float health;
+	public static float DAMAGE_COLOUR_TIME = 0.3f; // manually setting what should be consistent was annoying
 
 	public bool dead 		{ get; set; }
 	public bool grounded 	{ get; set; }
 	public bool facingRight { get; set; }
 
-	protected virtual void Update ()
+	protected bool invulnerable;
+	protected SpriteRenderer sprite;
+
+	protected virtual void Awake()
+	{
+		sprite = GetComponent<SpriteRenderer>();
+	}
+
+	protected virtual void Update()
 	{
 		if (health <= 0)
 		{
-			Kill ();
+			Kill();
 		}
 	}
 
-	public virtual void Kill () 
+	public virtual void Kill()
 	{
-		dead = true;
-		Destroy (gameObject);
+		dead = true; // what's the point of setting a var when it will be removed and inaccessible just after...
+		Destroy(gameObject); 
 	}
 
-	public virtual void Damage (float dmgTaken)
+	public virtual void Damage(float dmgTaken, GameComponent gc)
 	{
-		health -= dmgTaken;
+		if (!invulnerable && OwnedBy != gc.OwnedBy) 
+		{
+			health -= dmgTaken;
+			StartCoroutine("DamageColour");
+		}
+	}
+
+	IEnumerator DamageColour() {
+		invulnerable = true;
+		SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+		sprite.color = Color.red;
+		yield return new WaitForSeconds(DAMAGE_COLOUR_TIME);
+		sprite.color = Color.clear;
+		invulnerable = false;
 	}
 }

04141abcd83e16e70b20ea3ab1be3f6731899690
Froff <Fridtjof_Eik@hotmail.com>
2015-03-25 00:22:15 +0100

    If you squint the right way, you might play this without realizing it's broken

--- Assets/Resources/Game/Scripts/Forces/Gravity/GravityPulled.cs
+++ Assets/Resources/Game/Scripts/Forces/Gravity/GravityPulled.cs
@@ -1,10 +1,11 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 [RequireComponent(typeof(Rigidbody2D))]
 public class GravityPulled : MonoBehaviour 
 {
 	public bool keepUpright = true;
-	
+	public float uprightRange = 2.0f;
+
 	GameObject[] attractors;
 	GameObject closest;
 
@@ -34,7 +35,8 @@ public class GravityPulled : MonoBehaviour
 		}
 	}
 
-	private void FindClosest() {
+	private void FindClosest()
+	{
 		float biggestAtt = 0;
 		foreach(GameObject attractor in attractors)
 		{
@@ -50,13 +52,13 @@ public class GravityPulled : MonoBehaviour
 	
 	public void Attract(GameObject attractor)
 	{
-		GetComponent<Rigidbody2D>().AddForce(AttractForce(attractor));
+		GetComponent<Rigidbody2D> ().AddForce (AttractForce (attractor));
 	}
 	
 	public Vector2 AttractForce(GameObject attractor)
 	{
-		Debug.DrawLine(transform.position, attractor.transform.position, Color.blue);
-		Debug.DrawRay(transform.position, -(transform.position - attractor.transform.position).normalized, Color.red);
+	//	Debug.DrawLine(transform.position, attractor.transform.position, Color.blue);
+	//	Debug.DrawRay(transform.position, -(transform.position - attractor.transform.position).normalized, Color.red);
 		
 		return 
 			(attractor.transform.position - transform.position).normalized
@@ -68,7 +70,19 @@ public class GravityPulled : MonoBehaviour
 	public void KeepUpright(GameObject attractor) //Keep all calls of this function to Update, not FixedUpdate, or chracter will appear wrongly rotated when moving
 	{
 		Vector2 dir = new Vector2(attractor.transform.position.x - transform.position.x, attractor.transform.position.y - transform.position.y).normalized;
-		float angle = Vector2.Angle(Vector2.right, dir)*Mathf.Deg2Rad*Mathf.Sign(dir.y) + Mathf.PI/2;
-		transform.rotation = new Quaternion(0, 0, 1 * Mathf.Sin (angle/2), Mathf.Cos (angle/2));
+		if (Physics2D.OverlapCircle(transform.position, uprightRange, 1 << 9) != null)
+		{
+			RaycastHit2D hit = Physics2D.Raycast (transform.position, -transform.up, uprightRange, 1 << 9);
+			if (hit != null)
+			{
+				if (hit.normal != -Vector2.up)
+				{
+					dir = -hit.normal;
+				}
+			}
+		}
+
+		float angle = Vector2.Angle(Vector2.right, dir) * Mathf.Deg2Rad * Mathf.Sign(dir.y) + Mathf.PI/2;
+		transform.rotation = new Quaternion(0, 0, Mathf.Sin (angle/2), Mathf.Cos (angle/2));
 	}
 }
